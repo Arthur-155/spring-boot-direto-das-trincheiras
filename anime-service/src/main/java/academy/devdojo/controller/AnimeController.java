bc2397b1ce35d6a84d494b933302a141f88a33f6
Arthur <arthur_bergamo@hotmail.com>
2025-11-16 19:53:56 -0300

    Aula 15

--- anime-service/src/main/java/academy/devdojo/controller/AnimeController.java
+++ anime-service/src/main/java/academy/devdojo/controller/AnimeController.java
@@ -5,6 +5,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+import java.util.concurrent.ThreadLocalRandom;
 
 @RestController
 @RequestMapping("v1/animes")
@@ -12,7 +13,7 @@ public class AnimeController {
 
     @GetMapping
     public List<Anime> ListAll(@RequestParam(required = false) String nome) {
-        var animes = Anime.animeList();
+        var animes = Anime.animeList;
         if(nome == null) return animes;
 
         return animes.stream().filter(anime -> anime.getName().equalsIgnoreCase(nome)).toList();
@@ -20,9 +21,17 @@ public class AnimeController {
 
     @GetMapping("{id}")
     public Anime findById(@PathVariable Long id) {
-        return Anime.animeList().stream()
+        return Anime.animeList.stream()
                 .filter(anime -> anime.getId().equals(id))
                 .findFirst().orElse(null);
+    }
+
+    @PostMapping
+    public Anime createAnime(@RequestBody Anime anime){
+        Long id = ThreadLocalRandom.current().nextLong(1,1000);
+        anime.setId(id);
+        Anime.animeList.add(anime);
+        return anime;
     }
 }
 
--- anime-service/src/main/java/academy/devdojo/domain/Anime.java
+++ anime-service/src/main/java/academy/devdojo/domain/Anime.java
@@ -4,20 +4,24 @@ import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.Setter;
 import org.springframework.stereotype.Component;
+
+import java.util.ArrayList;
 import java.util.List;
 
+@Setter
 @Getter
 @AllArgsConstructor
 public class Anime {
     private Long id;
     private String name;
 
-    public static List<Anime>animeList(){
-        var katekyoHitmanReborn = new Anime(1L, "Katekyo Hitman Reborn");
-        var CodeGeass = new Anime(2L, "Code Geass");
-        var Pokemon = new Anime(3L, "Pokémon");
-        return List.of(katekyoHitmanReborn,CodeGeass,Pokemon);
-    }
+    public static List<Anime>animeList = new ArrayList<>(
+        List.of(
+                new Anime(1L, "Katekyo Hitman Reborn"),
+                new Anime(2L, "Code Geass"),
+                new Anime(3L, "Pokémon")
+        )
+    );
 
 
 }
